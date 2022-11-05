47b874487a85195444f7faf8b50066d37da94802
neilaben <97635019+neilaben@users.noreply.github.com>
2022-11-05 15:29:24 -0400

    Add files via upload
    Function Creating Newton's Method 
    df is the derived function in an m file.

---
+++ df.m
@@ -0,0 +1,6 @@
+function y = df(x)
+y = 3*x^2 + (6*);
+
+
+%function y = f(x)
+%y = x^3 -(3*x^2)/(2^x) + (3*x)/(4^x) -1/(8^x);
---
+++ newtonMethod.m
@@ -0,0 +1,19 @@
+% Newton’s method in general
+% excludes zero roots!
+steps = 0; % iteration counter
+x = input( 'Initial guess'  ); % estimate of root
+re = 1e-8; % required relative error
+myrel = 1;
+while myrel > re & (steps < 20)
+xold = x;
+x = x - f(x)/df(x);
+steps = steps + 1;
+disp( [x f(x)] )
+myrel = abs((x-xold)/x);
+end
+if myrel <= re
+disp( 'Zero found at' )
+disp( x )
+else
+disp( 'Zero NOT found' )
+end
